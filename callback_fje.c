47eedef434ee50f9708cf54960ec076c1f730742
milicagaljak <milicagaljakk@gmail.com>
2020-06-12 19:52:21 +0200

    endgame

--- callback_fje.c
+++ callback_fje.c
@@ -110,6 +110,15 @@ void on_display(void){
                 
             }   
             
+            glRasterPos3f(-2, 13, 5);
+            
+            for(int i = 0; i < len; i++){
+        
+            
+                glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18,score_string[i]);
+                
+            }
+            score = 0;
     }
     
     
@@ -256,6 +265,14 @@ void check_collision(){
             
             /***************************************/
             game_ongoing = 0;
+            speed_parametar = 0;
+            stone_initialize();
+            speed_player = 0;
+            x_player = 0;
+            y_player = 0.8;
+            z_player = 40;
+            draw_player();
+            glutPostRedisplay();
             glutPostRedisplay();
         }
         
